306fcf13960dfa2d4c3cb9d1781bce6c3bd4d171
Joseph <joseph@osdev.ninja>
2018-11-24 09:43:33 +0000

    Rename to jailfs

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ int main(int argc, char **argv) {
    if ((conf.log_fp = log_open(dconf_get_str("path.log", NULL))) == NULL)
       conf.log_fp = stdout;
 
-   Log(LOG_INFO, "fs-pkg: Package filesystem %s starting up...", VERSION);
+   Log(LOG_INFO, "jailfs: Package filesystem %s starting up...", VERSION);
    Log(LOG_INFO, "Copyright (C) 2012-2018 bigfluffy.cloud -- See LICENSE in distribution package for terms of use");
 
    if (conf.log_level == LOG_DEBUG)
